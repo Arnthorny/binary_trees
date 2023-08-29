10be304d381d4e7e5a1bf09bb8834a056d67d8d0
Cipher Phantom <oyepitan10@gmail.com>
2023-08-29 15:12:48 +0100

    Task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ *
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ *
+ * Return: The common ancestor and NULL if not found.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *parent1, *parent2;
+
+	if (first == second)
+		return (first->parent);
+	parent1 = (binary_tree_t *)first;
+	while (parent1)
+	{
+		parent2 = (binary_tree_t *)second;
+		while (parent2)
+		{
+			if (parent1 == parent2)
+				return (parent1);
+			parent2 = parent2->parent;
+		}
+		parent1 = parent1->parent;
+	}
+	return (NULL);
+}
