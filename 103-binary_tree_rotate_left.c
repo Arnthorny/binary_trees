d5956e652a2093143fe194a72ee8acf975fb3013
Arnthorny <chidubem.anthony26@gmail.com>
2023-08-30 19:22:51 -0700

    Betty Checks

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -5,7 +5,6 @@
 /**
  * binary_tree_rotate_left - Function to perform left-rotate on a tree
  * @tree: Tree to be rotated.
- * @func: A function used to operate on each node of tree.
  * Return: Pointer to new root node of rotated tree.
  * Description: When a tree(node) is left-rotated, it becomes the
  * left child of its right child and then bears the left
@@ -13,17 +12,17 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *tmp = NULL;
-	
-	if (tree == NULL || !(tmp = tree->right))
+	binary_tree_t *tmp = tree ? tree->right : NULL;
+
+	if (tree == NULL || !tmp)
 		return (tree);
-	
+
 	tree->right = tmp->left;
 	if (tmp->left)
 		tmp->left->parent = tree;
-	
+
 	tmp->left = tree;
-	
+
 	tmp->parent = tree->parent;
 	tree->parent = tmp;
 	return (tmp);
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -5,7 +5,6 @@
 /**
  * binary_tree_rotate_right - Function to perform right-rotate on a tree
  * @tree: Tree to be rotated.
- * @func: A function used to operate on each node of tree.
  * Return: Pointer to new root node of rotated tree.
  * Description: When a tree(node) is right-rotated, it becomes the
  * right child of its left child and then bears the right child
@@ -13,17 +12,17 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *tmp = NULL;
-	
-	if (tree == NULL || !(tmp = tree->left))
+	binary_tree_t *tmp = tree ? tree->right : NULL;
+
+	if (tree == NULL || !tmp)
 		return (tree);
-	
+
 	tree->left = tmp->right;
 	if (tmp->right)
 		tmp->right->parent = tree;
-	
+
 	tmp->right = tree;
-	
+
 	tmp->parent = tree->parent;
 	tree->parent = tmp;
 	return (tmp);
